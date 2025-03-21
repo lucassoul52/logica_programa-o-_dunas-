ef69c8e15bf285d25229a0eaabc4e21e3eee395e
JUVENALDO FLORENTINO CANJA <61490327363@professores.unifanor.edu.br>
2025-03-21 10:29:05 -0300

    atualizando o reporsitor

---
+++ primeiroProgama.py
@@ -0,0 +1,8 @@
+#utilizando a variavel n1, recebendo a função int e depois a função input recebendo a fala digite um número.
+n1 = int(input('Diga um númer:'))
+# utilizando a variavel n2, recebendo a função int e depois a função input recebendo a fala digite outro45 número.
+n2 = int(input('Digite outro número:'))
+# usando função soma para soma as variaveis de n1 e n2.
+s = n1 + n2 
+#usando a função print para escrever e no meio chaves para ser colocado a função .format amostrando as vaiaveis de n1, n2 e a soma.
+print('o resultado da soma do número {} e {} é {}!'.format(n1, n2, s))
---
+++ variaveis.py
@@ -0,0 +1,4 @@
+# criação e declaração de variavel
+nome = 'lucas'
+n1 = 458
+print(f'o teu nome é {nome} e a tua matricula {n1}')
